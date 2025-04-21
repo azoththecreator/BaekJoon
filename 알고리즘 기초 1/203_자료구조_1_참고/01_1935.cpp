c02959866aea4e032641cdcf47d87653957dc96e
azoththecreator <flod1998@naver.com>
2025-04-21 17:05:46 +0900

    201 203

---
+++ 알고리즘 기초 1/203_자료구조_1_참고/01_1935.cpp
@@ -0,0 +1,88 @@
+/*
+
+1935. 후위 표기식2
+
+[문제]
+후위 표기식과 각 피연산자에 대응하는 값들이 주어져 있을 때, 그 식을 계산하는 프로그램을 작성하시오.
+
+[입력]
+첫째 줄에 피연산자의 개수(1 ≤ N ≤ 26) 가 주어진다.
+그리고 둘째 줄에는 후위 표기식이 주어진다. (여기서 피연산자는 A~Z의 영대문자이며, A부터 순서대로 N개의 영대문자만이 사용되며, 길이는 100을 넘지 않는다)
+그리고 셋째 줄부터 N+2번째 줄까지는 각 피연산자에 대응하는 값이 주어진다.
+3번째 줄에는 A에 해당하는 값, 4번째 줄에는 B에 해당하는값 , 5번째 줄에는 C ...이 주어진다.
+그리고 피연산자에 대응 하는 값은 100보다 작거나 같은 자연수이다.
+
+후위 표기식을 앞에서부터 계산했을 때, 식의 결과와 중간 결과가 -20억보다 크거나 같고, 20억보다 작거나 같은 입력만 주어진다.
+
+[출력]
+계산 결과를 소숫점 둘째 자리까지 출력한다.
+
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string eq;
+    cin >> eq;
+
+    unordered_map<char, int> var;
+    for (int i = 0; i < n; ++i)
+    {
+        int tmp;
+        cin >> tmp;
+        var['A' + i] = tmp;
+    }
+
+    stack<double> s;
+    for (int i = 0; i < eq.size(); ++i)
+    {
+        double tmp1, tmp2;
+        switch (eq[i])
+        {
+            case ('+'):
+                tmp1 = s.top();
+                s.pop();
+                tmp2 = s.top();
+                s.pop();
+                s.push(tmp2 + tmp1);
+                break;
+            case ('-'):
+                tmp1 = s.top();
+                s.pop();
+                tmp2 = s.top();
+                s.pop();
+                s.push(tmp2 - tmp1);
+                break;
+            case ('*'):
+                tmp1 = s.top();
+                s.pop();
+                tmp2 = s.top();
+                s.pop();
+                s.push(tmp2 * tmp1);
+                break;
+            case ('/'):
+                tmp1 = s.top();
+                s.pop();
+                tmp2 = s.top();
+                s.pop();
+                s.push(tmp2 / tmp1);
+                break;
+            default:
+                s.push(var[eq[i]]);
+        }
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << s.top();
+
+    return 0;
+}
